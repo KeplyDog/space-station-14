1b17f843110d889a145ba2a8bf8dd076f02ecd10
BS <--replace>
2024-11-02 09:27:55 +1000

    ADD: cooldown in ServerMessage

--- Content.Server/Administration/Commands/ServerMessageCommand.cs
+++ Content.Server/Administration/Commands/ServerMessageCommand.cs
@@ -1,8 +1,7 @@
-using Content.Server.Administration;
-using Content.Server.Chat;
 using Content.Server.Chat.Systems;
 using Content.Shared.Administration;
 using Robust.Shared.Console;
+using Robust.Shared.Timing;
 
 namespace Content.Server.Administration.Commands
 {
@@ -12,9 +11,19 @@ namespace Content.Server.Administration.Commands
         public string Command => "servermessage";
         public string Description => "Send an in-game server message.";
         public string Help => $"{Command} <int times> <message> or {Command} <message> to send message.";
+        private IGameTiming _timing = default!;
+        private readonly TimeSpan _cooldown = TimeSpan.FromSeconds(5);
+        private TimeSpan _repentTime = TimeSpan.Zero;
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             var chat = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<ChatSystem>();
+            _timing = IoCManager.Resolve<IGameTiming>();
+
+            if (_timing.CurTime < _repentTime)
+            {
+                shell.WriteLine($"Cooldown. You can write it after {_repentTime.Seconds - _timing.CurTime.Seconds} seconds.");
+                return;
+            }
 
             if (args.Length == 0)
             {
@@ -26,6 +35,7 @@ namespace Content.Server.Administration.Commands
             {
                 chat.DispatchServerMessage(args[0], colorOverride: Color.Cyan);
                 shell.WriteLine("Sent!");
+                _repentTime = _cooldown + _timing.CurTime;
             }
             else
             {
@@ -35,6 +45,7 @@ namespace Content.Server.Administration.Commands
                     chat.DispatchServerMessage(message, colorOverride: Color.Cyan);
                 }
                 shell.WriteLine("Sent!");
+                _repentTime = _cooldown + _timing.CurTime;
             }
         }
     }
