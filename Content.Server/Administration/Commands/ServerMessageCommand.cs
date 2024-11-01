8fbab0859306c7e47f3578d9a45bddc6f82c5f53
BS <--replace>
2024-11-02 00:06:16 +1000

    CHANGE: ServerMessage

--- Content.Server/Administration/Commands/ServerMessageCommand.cs
+++ Content.Server/Administration/Commands/ServerMessageCommand.cs
@@ -6,12 +6,12 @@ using Robust.Shared.Console;
 
 namespace Content.Server.Administration.Commands
 {
-    [AdminCommand(AdminFlags.Moderator)]
+    [AdminCommand(AdminFlags.Host)]
     public sealed class ServerMessageCommand : IConsoleCommand
     {
         public string Command => "servermessage";
         public string Description => "Send an in-game server message.";
-        public string Help => $"{Command} <message> to send message.";
+        public string Help => $"{Command} <int times> <message> or {Command} <message> to send message.";
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             var chat = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<ChatSystem>();
@@ -22,8 +22,20 @@ namespace Content.Server.Administration.Commands
                 return;
             }
 
-            chat.DispatchServerMessage(args[0], colorOverride: Color.Cyan);
-            shell.WriteLine("Sent!");
+            if (args.Length == 1 || !int.TryParse(args[0], out int times))
+            {
+                chat.DispatchServerMessage(args[0], colorOverride: Color.Cyan);
+                shell.WriteLine("Sent!");
+            }
+            else
+            {
+                var message = string.Join(' ', new ArraySegment<string>(args, 1, args.Length - 1));
+                for (int i = 0; i < times; i++)
+                {
+                    chat.DispatchServerMessage(message, colorOverride: Color.Cyan);
+                }
+                shell.WriteLine("Sent!");
+            }
         }
     }
 }
